97efa9c3d0bfa1cfe875b24127445919a613f24a
marksindustries <prohan747@gmail.com>
2023-04-14 18:59:05 +0530

    Added login router

--- routers/unnatiClients.py
+++ routers/unnatiClients.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 import models
 from schemas import CreateClient
 from fastapi.responses import Response
+from fastapi.exceptions import HTTPException
 
 
 router = APIRouter(
@@ -16,6 +17,14 @@ router = APIRouter(
 async def getAllCustomer(db:Session = Depends(get_db)):
     all = db.query(models.Client).all()
     return all
+
+@router.post('/login/{user_id}/{password}')
+async def login(user_id:int,password:str,db:Session = Depends(get_db)):
+    user = db.query(models.Client).filter(models.Client.clientUserId == user_id).filter(models.Client.password == password).first()
+    if not user:
+        raise HTTPException(status_code=404)
+    return Response(status_code=200)
+
 
 
 @router.post("/createClients")
